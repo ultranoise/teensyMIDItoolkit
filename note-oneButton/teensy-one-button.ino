9e596c01f1d68666bc9e8406ef337232abe95f91
Enrique Tomás -ultranoise <ultranoise@users.noreply.github.com>
2022-03-04 19:07:24 +0100

    Add files via upload

---
+++ note-oneButton/teensy-one-button.ino
@@ -0,0 +1,65 @@
+/* Buttons to USB MIDI Example
+
+   You must select MIDI from the "Tools > USB Type" menu
+
+   To view the raw MIDI data on Linux: aseqdump -p "Teensy MIDI"
+
+   This example code is in the public domain.
+*/
+
+#include <Bounce.h>
+
+// the MIDI channel number to send messages
+const int channel = 1;
+
+// Create Bounce objects for each button.  The Bounce object
+// automatically deals with contact chatter or "bounce", and
+// it makes detecting changes very simple.
+Bounce button0 = Bounce(0, 5);
+
+
+void setup() {
+  // Configure the pins for input mode with pullup resistors.
+  // The pushbuttons connect from each pin to ground.  When
+  // the button is pressed, the pin reads LOW because the button
+  // shorts it to ground.  When released, the pin reads HIGH
+  // because the pullup resistor connects to +5 volts inside
+  // the chip.  LOW for "on", and HIGH for "off" may seem
+  // backwards, but using the on-chip pullup resistors is very
+  // convenient.  The scheme is called "active low", and it's
+  // very commonly used in electronics... so much that the chip
+  // has built-in pullup resistors!
+  pinMode(0, INPUT_PULLUP);
+}
+
+void loop() {
+  // Update all the buttons.  There should not be any long
+  // delays in loop(), so this runs repetitively at a rate
+  // faster than the buttons could be pressed and released.
+  button0.update();
+
+  // Check each button for "falling" edge.
+  // Send a MIDI Note On message when each button presses
+  // Update the Joystick buttons only upon changes.
+  // falling = high (not pressed - voltage from pullup resistor)
+  //           to low (pressed - button connects pin to ground)
+  if (button0.fallingEdge()) {
+    usbMIDI.sendNoteOn(60, 99, channel);  // 60 = C4
+  }
+
+  // Check each button for "rising" edge
+  // Send a MIDI Note Off message when each button releases
+  // For many types of projects, you only care when the button
+  // is pressed and the release isn't needed.
+  // rising = low (pressed - button connects pin to ground)
+  //          to high (not pressed - voltage from pullup resistor)
+  if (button0.risingEdge()) {
+    usbMIDI.sendNoteOff(60, 0, channel);  // 60 = C4
+  }
+
+  // MIDI Controllers should discard incoming MIDI messages.
+  // http://forum.pjrc.com/threads/24179-Teensy-3-Ableton-Analog-CC-causes-midi-crash
+  while (usbMIDI.read()) {
+    // ignore incoming messages
+  }
+}
